5219c9eecf39d0e172cd5fd5b77c3c1f5542903c
Richard <200463605@student.georgianc.on.ca>
2022-09-21 10:30:47 -0400

    Lab 1 System Info Script

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+#Prints computers FQDN
 echo 'Domain Name:'
 hostname --fqdn
 
+#Prints information about computer
 echo 'Host Info:'
 hostnamectl
 
+#Prints every IP address except for IPs on the 127. network
 echo 'IP Addresses:'
 hostname -I | grep -v 127.*
 
+#Prints disk space of Root
 echo 'Root Filesystem Size:'
 df -h /
