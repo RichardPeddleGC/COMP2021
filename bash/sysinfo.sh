226c4a0450d61d3581fbfb995d51d61b9981a881
Richard <200463605@student.georgianc.on.ca>
2022-09-28 11:23:08 -0400

    Lab 2  System Info Script

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+#Assigns hostname to ComputerName varible
 ComputerName=$(hostname --short)
-
+#Assign FQDN name to FQDNName Variable
 FQDNName=$(hostname --fqdn)
-
+#Searches for Operating System in hostnamectl, assigns to OperatingSys variable
 OperatingSys=$(hostnamectl | grep "Operating")
-
-
+#Searches for line that contains IP address, isolates IP address and assigns to variable
 IpAddress=$(ip route | grep "link src" | awk '{print $9}')
-
-RootFileSpace=$(df -h / | awk '{print $4}' | grep -v "Avail")
+#Searches for line that shows the space, isolates line to text containing free space, assigns to variable RootFileSpace
+RootFileSpace=$(df -h / | grep -v "Avail" |  awk '{print $4}')
 
 #Prints out the data in a report
 cat <<EOF
